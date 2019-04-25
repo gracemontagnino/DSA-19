fbc9f0dd61f93e66522ad03b2288e888fb0fb5b2
gracemontagnino <grace.montagnino@students.olin.edu>
2019-04-24 20:47:33 -0400

    dp01 done

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,10 +1,29 @@
+import java.util.HashMap;
 public class DiceRollSum {
+    //Sub problem N-1
+    //Guess what roll do we need to get next?
+    //Recurrence Relation recursion on all six sides
+    //Memoize if map has key
+    //Solve return DP
+    // Runtime:O(N)
+    // Space: O(N)
+    public  static int diceRollSum(int N) {
+        HashMap <Integer,Integer> map =new HashMap<>();
+        return recursive(N,map);
 
-    // Runtime: TODO
-    // Space: TODO
-    public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+    }
+    public static int recursive(int N,HashMap<Integer,Integer> map) {
+        if (N==0) return 1;
+        if (N<0) return 0;
+        if (map.containsKey(N)){
+            return map.get(N);
+        }
+        int DP=recursive(N-1,map)+recursive(N-2,map)+recursive(N-3,map)+recursive(N-4,map)+recursive(N-5,map)+recursive(N-6,map);
+        map.put(N,DP);
+        return DP;
+        }
+
+
     }
 
-}
+
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,35 @@
-public class LongestIncreasingSubsequence {
+import java.util.Arrays;
 
-    // Runtime: TODO
-    // Space: TODO
+public class LongestIncreasingSubsequence {
+    //Sub problem Largest Sub sequence @ i
+    //Guess Is next number larger?
+    //Recurrence Relation max prev and next
+    //Memoize if DP!=-1
+    //Solve return DP[i]
+    // Runtime: A.length*A.length-1=O(n^2)
+    // Space: O(N)
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        int[] DP= new int [A.length];
+        for (int i = 0; i < A.length; i++) {
+            DP[i] = -1;
+        }
+        int ans=0;
+        for (int i = 0; i < A.length; i++) {
+            ans=Math.max(recursive(DP,A,i),ans);
+        }
+        return ans;
+
+        }
+
+        public static int recursive(int[] DP, int[] A, int i) {
+            if (DP[i]!=-1)return DP[i];
+            int ansr=1;
+            for(int j=i+1;j<A.length;j++)
+            {if(A[j]>A[i]){ansr=Math.max(ansr,1+recursive(DP,A,j));}
+               }
+            DP[i]=ansr;
+            return DP[i];
+        }
+
+
     }
-}
